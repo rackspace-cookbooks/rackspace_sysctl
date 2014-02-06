e19b56fdac64647f1d04c7829dddcb21a3c56584
Ryan Richard <ryanrichard07@gmail.com>
2014-02-06 13:35:39 -0600

    house keeping

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ name             'rackspace_sysctl'
 license          'Apache v2.0'
 description      'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '1.0.0'
+
 %w{ debian ubuntu centos redhat }.each do |os|
   supports os
 end
